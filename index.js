7098ad1a8129381a88f86ad0a599e72d1cd07d24
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-12 01:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function keepGoing(array, changeValue, skipValue){
 }
 function findBy(array, findFn){
   for ( let i=0; i<array.length; i++){
-    if (findFn(array[i])) === 1) {
+    if (findFn(array[i])) {
       return array;
 }
